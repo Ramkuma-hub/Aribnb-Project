116299efa7c6ff6a3e1486240616ecc5ff90cd56
Ramkuma-hub <ramkumarmanjhi68@gmail.com>
2026-03-21 21:43:37 +0530

    Initial commit: Wonderlust project

--- middleware.js
+++ middleware.js
@@ -1,67 +1,65 @@
 import Listing from "./model/listing.js";
-import { ExpressErr } from './utils/Expresserr.js';
-import { listschema, reviewSchema, } from './schemajoi.js';
+import { ExpressErr } from "./utils/Expresserr.js";
+import { listschema, reviewSchema } from "./schemajoi.js";
 import Review from "./model/Review.js";
 
 export const loggedIn = (req, res, next) => {
-    if (!req.isAuthenticated()) {
-        req.session.redirectUrl = req.originalUrl;
-        req.flash("error", "You must be logged in");
-        return res.redirect("/login");
-    }
-    else {
-        next();
-    }
-}
+  if (!req.isAuthenticated()) {
+    req.session.redirectUrl = req.originalUrl;
+    req.flash("error", "You must be logged in");
+    return res.redirect("/login");
+  } else {
+    next();
+  }
+};
 
 export const saveurl = (req, res, next) => {
-    if (req.session.redirectUrl) {
-        res.locals.redirectUrl = req.session.redirectUrl;
-    }
-    next()
-}
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl;
+  }
+  next();
+};
 export const owneracess = async (req, res, next) => {
-    const { id } = req.params;
-    const listing = await Listing.findById(id);
-    if (!listing.owner.equals(res.locals.currentUser._id)) {
-        req.flash("error", "you are not owner of the list")
-        return res.redirect(`/listings/${id}`)
-    }
-    next()
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
 
-}
+  if (!res.locals.currUser) {
+    req.flash("error", "Your session has expired. Please log in again.");
+    return res.redirect("/login");
+  }
 
+  if (!listing.owner.equals(res.locals.currUser._id)) {
+    req.flash("error", "You are not the owner of this listing");
+    return res.redirect(`/listings/${id}`);
+  }
+  next();
+};
 
 export const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        let msg = error.details.map(el => el.message).join(',');
-        throw new ExpressErr(msg, 400);
-    }
-    next();
-}
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressErr(msg, 400);
+  }
+  next();
+};
 
 export const validateListing = (req, res, next) => {
-    const { error } = listschema.validate(req.body.listing);
-    if (error) {
-        let msg = error.details.map(el => el.message).join(',');
-        throw new ExpressErr(msg, 400);
-    }
+  const { error } = listschema.validate(req.body.listing);
+  if (error) {
+    let msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressErr(msg, 400);
+  }
 
-    next();
-}
+  next();
+};
 
 export const isreviewvalid = async (req, res, next) => {
-    const { id, reviewId } = req.params
-    const review = await Review.findById(reviewId)
-    if (!review.author.equals(res.locals.currentUser._id)) {
-        req.flash("error", "you are not owner of this review")
-        return res.redirect(`/listings/${reviewId}`)
-
-    }
-    next()
-
-}
-
-
-
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review.author.equals(res.locals.currentUser._id)) {
+    req.flash("error", "you are not owner of this review");
+    return res.redirect(`/listings/${reviewId}`);
+  }
+  next();
+};
