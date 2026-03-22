98fd11c50936f8e9ad20fd5c0168b395a3ffc18b
Ramkuma-hub <ramkumarmanjhi68@gmail.com>
2026-03-22 17:50:21 +0530

    Update userroute.js

--- route/userroute.js
+++ route/userroute.js
@@ -6,6 +6,10 @@ const router = express.Router();
 import passport from 'passport';
 import { saveurl } from '../middleware.js';
 import { getlogin, getsignup, logout, postlogin, postsignup } from '../controller/usercontroller.js';
+import { index } from '../controller/listingcontroller.js';
+
+router.route("")
+.get(wrapAsync(index))
 
 
 router.route("/signup")
